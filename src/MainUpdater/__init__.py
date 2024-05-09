ad7e5671a7e03da91486e790bbb93cce922adf5b
MainPlay-TG <xbox.roman6666666666@gmail.com>
2024-05-09 14:00:17 +0700

    Run autopep8

--- src/MainUpdater/__init__.py
+++ src/MainUpdater/__init__.py
@@ -2,19 +2,19 @@
 \u0420\u0430\u0437\u0440\u0430\u0431\u043E\u0442\u0447\u0438\u043A: MainPlay TG
 https://t.me/MainPlay_InfoCh"""
 
-__version_tuple__=(0,0,1)
-__depends__={
-  "required":[
-    "requests",
+__version_tuple__ = (0, 0, 5)
+__depends__ = {
+    "required": [
+        "requests",
     ],
-  "optional":[
-    "progressbar2",
+    "optional": [
+        "progressbar2",
     ],
-  }
-__scripts__=[
-  "MU-updateAll",
-  ]
-__all__=[]
+}
+__scripts__ = [
+    "MU-updateAll",
+]
+__all__ = []
 from .main import *
 __all__.sort()
-__version__="{}.{}.{}".format(*__version_tuple__)
+__version__ = "{}.{}.{}".format(*__version_tuple__)
--- src/MainUpdater/script.py
+++ src/MainUpdater/script.py
@@ -1,8 +1,10 @@
 from MainUpdater.main import *
 from sys import argv
+
+
 def updateAll():
-  if len(argv)==1:
-    update_all(os.getcwd(),cli=True)
+  if len(argv) == 1:
+    update_all(os.getcwd(), cli=True)
   else:
     for i in argv[1:]:
-      update_all(i,cli=True)
+      update_all(i, cli=True)
